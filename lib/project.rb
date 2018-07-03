d67684b7a44ac83ba877c6e36dc281e29b3e0bcb
Nate Miller <natekmiller96@gmail.com>
2018-07-03 21:23:03 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,11 @@
+class Project
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @backers = []
+  end
+  def add_backer(backer)
+    @backers << project
+    backer.back_project(self)
+  end
+  def 
